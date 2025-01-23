6509dad18dce0a31cbe322eea8530a3cf23a73f7
Israel Mendoza <artisra@amazon.com>
2025-01-22 23:56:55 -0600

    27. Remove Element

---
+++ src/main/java/dev/artisra/topinterview150/RemoveElement.java
@@ -0,0 +1,28 @@
+package dev.artisra.topinterview150;
+
+/**
+ * Solution for removing all occurrences of a specified value from an array in-place.
+ * @see <a href="https://leetcode.com/problems/remove-element/description/">Problem Description on LeetCode</a>
+ */
+public class RemoveElement {
+    /**
+     * Removes all occurrences of a specified value from the array in-place.
+     * The relative order of the elements not equal to val is maintained.
+     *
+     * @param nums the input array to be modified
+     * @param val the value to be removed
+     * @return the number of elements in nums which are not equal to val
+     */
+    public int removeElement(int[] nums, int val) {
+        int availableIndex = 0;
+
+        for (int num : nums) {
+            if (num != val) {
+                nums[availableIndex] = num;
+                availableIndex++;
+            }
+        }
+
+        return availableIndex;
+    }
+}
